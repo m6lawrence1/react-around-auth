15201dcf408d3a17fb9217646a1f6841c7ef4a1b
Michael Lawrence <m6lawrence1@gmail.com>
2020-10-09 22:36:34 -0400

    fix for gh-pages

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,34 +1,46 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
 
 function PopupWithForm(props) {
-    
-    function handleOverlayClick(evt) {
-        if (evt.target.classList.contains('popup')) {
-          props.onClose();
-        }
-      }
-    
-    
-    
-    const popup = (
-    <div onClick={handleOverlayClick} className={`popup popup_type_${props.name} ${props.isOpen ? 'popup_opened' : ''}`}>
-      <div className={`popup__container ${props.name === 'delete' ? 'popup__container_type_confirmation' : ''}`}>
-        <button className="popup__close-button" onClick={props.onClose}></button>
+  function handleOverlayClick(evt) {
+    if (evt.target.classList.contains("popup")) {
+      props.onClose();
+    }
+  }
+
+  const popup = (
+    <div
+      onClick={handleOverlayClick}
+      className={`popup popup_type_${props.name} ${
+        props.isOpen ? "popup_opened" : ""
+      }`}
+    >
+      <div
+        className={`popup__container ${
+          props.name === "delete" ? "popup__container_type_confirmation" : ""
+        }`}
+      >
+        <button
+          className="popup__close-button"
+          onClick={props.onClose}
+        ></button>
         <h2 className="popup__title">{props.title}</h2>
-        <form action="#" className="form" name={props.name} onSubmit={props.onSubmit} noValidate>
-            {props.children}
-            <button type="submit" className="form__submit-button">{props.submitBtnText}</button>
+        <form
+          action="#"
+          className="form"
+          name={props.name}
+          onSubmit={props.onSubmit}
+          noValidate
+        >
+          {props.children}
+          <button type="submit" className="form__submit-button">
+            {props.submitBtnText}
+          </button>
         </form>
       </div>
     </div>
-    )
-    return ReactDOM.createPortal(
-         popup, document.getElementById("modal-root")
-    );
+  );
+  return ReactDOM.createPortal(popup, document.getElementById("root"));
 }
 
-
 export default PopupWithForm;
-
-
